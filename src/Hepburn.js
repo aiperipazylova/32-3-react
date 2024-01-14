5b91ec2d34fee8e6f5ab43e8121b3dc4c5a083e6
aiperipazylova <peripro96@gmail.com>
2024-01-14 14:03:04 +0100

    homework_1

--- src/Hepburn.js
+++ src/Hepburn.js
@@ -1,25 +1,3 @@
-// import React from "react";
-
-// const user = {
-//   name: "Audrey Hepburn",
-//   imageUrl:"https://i.pinimg.com/474x/a9/b2/13/a9b213f0d5a0f25768b2a73f5d7deb40.jpg",
-//   imageSize: 308,
-// };
-
-// export default function Profile() {
-//   return (
-//       <>
-//           <h1>{user.name}</h1>
-//           <img 
-//                className="avatar"
-//                src={user.imageUrl}
-//                alt={"photo of" + user.name}
-//                style={{width: user.imageSize, height: user.imageSize}}
-//           />
-//       </>
-//   )
-// }
-
 import React from "react";
 
 const user = {
